2e1c251f5067297c80fbbce132560196956bae7a
Gregory <hunter.gregorir@gmail.com>
2026-04-30 22:18:30 -0700

    Minor Bug fix 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     tags: ''
   });
 
-  const API_BASE_URL = 'https://linklibrarian-production.up.railway.app'; // Base URL for the deployed backend API. Change this to 'http://localhost:3001' for local development.
+  const API_BASE_URL = 'http://localhost:3001'; // Base URL for the deployed backend API. Change this to 'http://'https://linklibrarian-production.up.railway.app'' for public deployment.
 
   // Function to filter links based on the tag filter input.
   const [tagFilter, setTagFilter] = useState('');
